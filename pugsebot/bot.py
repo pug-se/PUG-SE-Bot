c02000258574fc175a2f70a7ad04d3ae7bfb8ddd
Wagner Macedo <wagnerluis1982@gmail.com>
2018-04-19 11:52:44 -0300

    Defined module pugsebot.bot
    - Customize the GDGAjuBot by removing easter eggs
    - Define our own class PugSeBot, inheriting GDGAjuBot

---
+++ pugsebot/bot.py
@@ -0,0 +1,9 @@
+import gdgajubot.bot
+
+# hack to remove irrelevant easter eggs
+gdgajubot.bot.easter_egg.functions.clear()
+
+
+# our own bot class
+class PugSeBot(gdgajubot.bot.GDGAjuBot):
+    pass
